2b4e73d30a76729433f626adedea592458ff8eea
David Martinez <davidf.martinez.br@gmail.com>
2024-05-21 17:54:37 -0500

    Static Data

---
+++ src/utils/data.js
@@ -0,0 +1,11 @@
+export const navItems = [
+	{ title: "Sobre mí", label: "sobre-mí", url: "/#sobre-mi" },
+	{ title: "Proyectos", label: "proyectos", url: "/#proyectos" },
+	{ title: "Experiencia", label: "experiencia", url: "/#experiencia" },
+	{ title: "Contacto", label: "contacto", url: "/#contacto" },
+];
+
+export const personalData = {
+	github: "https://github.com/dmzdev7",
+	linkedin: "https://www.linkedin.com/in/david-martinez-bravo",
+};
